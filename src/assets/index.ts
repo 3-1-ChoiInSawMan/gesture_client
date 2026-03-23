1da6a1dbf2a34a8ec97bdc9347e66c0bc62387d7
hwnsng <amc214677@gmail.com>
2026-03-23 14:33:45 +0900

    assets: 이미지 및 에셋 추가

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import Logo from './images/logo.png';
+import Kakao from './images/kakao.png';
+import Google from './images/google.png';
+import Naver from './images/naver.png';
+import DefaultProfile from './images/default_profile.png';
+
+export { Logo, Kakao, Google, Naver, DefaultProfile }

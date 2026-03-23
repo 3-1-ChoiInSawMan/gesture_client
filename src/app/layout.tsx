633088921e71bf93558de18abf6e19f43fcbd8dd
hwnsng <amc214677@gmail.com>
2026-03-23 14:33:45 +0900

    style: 전역 스타일 및 루트 레이아웃 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
+import { Toaster } from "react-hot-toast";
 import "@/styles/globals.css";
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="ko">
       <body>
+        <Toaster position="top-center" reverseOrder={false} />
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-
+import Link from 'next/link';
 
 export default function Home(){
   return(
-    <div>
-      인규 돼지
+    <div className="min-w-screen min-h-screen flex flex-col justify-center items-center">
+      <h1><Link href="/auth/login">로그인</Link></h1>
+      <h1><Link href="/auth/signup">회원가입</Link></h1>
     </div>
   );
 }
